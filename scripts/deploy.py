03f69dbc78eb2c0231daa7869a90c66d7aee7ed8
francisco <fr.betancourt@live.com>
2021-12-31 20:54:29 -0300

    Favorite number updated calling function in smart contract

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,7 +5,10 @@ def deploy_simple_storage():
     account = accounts[0]
     simple_storage = SimpleStorage.deploy({"from": account})
     stored_value = simple_storage.retrieve()
-    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
 
 
 def main():
