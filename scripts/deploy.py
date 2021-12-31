0da27f7d1cbc7ffd6f6c8c82207af2d2560a3be6
francisco <fr.betancourt@live.com>
2021-12-31 20:51:13 -0300

    Retrieving favorite number calling method in smart contract.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,9 +4,8 @@ from brownie import accounts, config, SimpleStorage
 def deploy_simple_storage():
     account = accounts[0]
     simple_storage = SimpleStorage.deploy({"from": account})
-    # Transact
-    # Call
-    print(simple_storage)
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
 
 
 def main():
