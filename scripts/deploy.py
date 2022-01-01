aed0c3a99663970bddb2a81218e140996c59c866
francisco <fr.betancourt@live.com>
2022-01-01 13:10:32 -0300

    Added function for getting account

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 
 def deploy_simple_storage():
@@ -9,6 +9,13 @@ def deploy_simple_storage():
     transaction.wait(1)
     updated_stored_value = simple_storage.retrieve()
     print(updated_stored_value)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
