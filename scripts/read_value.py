e95bd8a85f18b0987989501cd5f29fa0bf34d3fc
francisco <fr.betancourt@live.com>
2022-01-01 13:28:37 -0300

    Defined function for reading from the contract. We get the latest deployed contract.

---
+++ scripts/read_value.py
@@ -0,0 +1,12 @@
+from brownie import SimpleStorage, accounts, config
+
+
+def read_contract():
+    simple_storage = SimpleStorage[-1]
+    # go take the index thats one less than the length
+    # ABI
+    # Address
+
+
+def main():
+    read_contract()
